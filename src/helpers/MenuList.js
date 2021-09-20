e53eb6eb90146337b4ecf626de1a9b8a69fd9548
natan.esteves <fulanodetal@exemplo.br>
2021-09-20 10:51:04 -0300

    first commit

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,39 @@
+import Pepperoni from "../assets/emconstru.jpg";
+import Margherita from "../assets/emconstru.jpg";
+import PedroTechSpecial from "../assets/emconstru.jpg";
+import Vegan from "../assets/emconstru.jpg";
+import Pineapple from "../assets/emconstru.jpg";
+import Expensive from "../assets/emconstru.jpg";
+
+export const MenuList = [
+  {
+    name: "",
+    image: Pepperoni,
+   
+  },
+  {
+    name: "",
+    image: Margherita,
+   
+  },
+  {
+    name: "",
+    image: PedroTechSpecial,
+   
+  },
+  {
+    name: "",
+    image: Vegan,
+   
+  },
+  {
+    name: "",
+    image: Pineapple,
+    
+  },
+  {
+    name: "",
+    image: Expensive,
+   
+  },
+];
---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import BannerImage from "../assets/emconstru.jpg";
+import "../styles/Home.css";
+
+function Home() {
+  return (
+    <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
+      <div className="headerContainer">
+        <h1> Site Daniel </h1>
+        <p> Musica de qualidade</p>
+        <Link to="/menu">
+          <button> Mais Sobre </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
