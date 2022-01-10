0bdeed769a66f692f6930cf5e285f0e11ddae697
natan.esteves <fulanodetal@exemplo.br>
2022-01-09 21:26:48 -0300

    update

--- src/lib/components/Footer.js
+++ src/lib/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import InstagramIcon from "@material-ui/icons/Instagram";
 
 import FacebookIcon from "@material-ui/icons/Facebook";
@@ -12,6 +13,7 @@ function Footer() {
       <div className="socialMedia">
         <a href="https://www.instagram.com/danielhornmusic/"><InstagramIcon/></a> 
         <a href="https://www.facebook.com/DaniHornMusic"><FacebookIcon /></a>  
+        
       </div>
       <p className="footinho"> &copy;danielhornmusic@gmail.com</p>
       <p className="footinho2"> &copy;Desenvolvido por: //github.com/natanesteves</p>
